d078ff5c538f66b7018e58f46e355d9ad739fc4a
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-08-26 17:49:50 -0300

    Ajustes na página inicial

--- src/main.js
+++ src/main.js
@@ -114,10 +114,6 @@ function mainFunction(data) {
     printCardsGeneric(charactersByName);
   }
   searchInput.addEventListener("keyup", searchByName);
-
-  const cursor = document.querySelector('.cursor');
-// cursor
-  document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: "+(e.pageX - 15) + "px;"); 
-  })
 }
+
+
